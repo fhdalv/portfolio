838b3b5b5d65ae345c3a0c5c52f746fb580fdb53
Fahad Alvi <fahad994@gmail.com>
2024-10-09 21:39:59 -0400

    Update

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const gitApi = {
+    "login": "fhdalv",
+    "id": 31874920,
+    "node_id": "MDQ6VXNlcjMxODc0OTIw",
+    "avatar_url": "https://avatars.githubusercontent.com/u/31874920?v=4",
+    "name": "Fahad Alvi",
+    "company": null,
+    "blog": "https://www.facebook.com/fhdalv",
+    "location": "New York",
+    "email": null,
+    "hireable": true,
+    "bio": "An aspiring software developer.",
+    "updated_at": "2024-10-08T17:40:50Z"
+  }
+  document.getElementById("gitBio").innerHTML = gitApi.bio; 
+
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".burger-icon");
